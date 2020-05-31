e8220afbdc36285688666c135b3ecaa3917b4d30
Luke McCann <developer-lukemccann@outlook.com>
2020-05-31 16:33:45 +0100

    Basic hello world

---
+++ html/helloworld.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php 
+            echo "Hello World!" . " Where is the bacon?";
+        ?>
+    </body>
+</html>
