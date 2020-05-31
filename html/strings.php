a21ae4126536e2aa32088d577d60e2fa9691433f
Luke McCann <developer-lukemccann@outlook.com>
2020-05-31 17:10:00 +0100

    Add string methods example

--- html/strings.php
+++ html/strings.php
@@ -15,7 +15,30 @@
         ?>
         <br />
         <?php
-            echo "$phrase Again<br />";
+            echo "{$phrase} Again<br />";
+            echo "{\$phrase} Again<br />"
         ?>
+        <br />
+        <br />
+        <?php 
+            echo "Functions: <br /><br />";
+            echo "Initial string: ";
+            $first = "The quick brown fox";
+            $second = " jumped over the lazy dog.";
+
+            $third = $first;
+            # concatenate and assign
+            $third .= $second;
+            echo $third;
+        ?>
+        <br />
+        Lowercase: <?php echo strtolower($third);?>
+        <br />
+        Uppercase: <?php echo strtoupper($third);?>
+        <br />
+        Uppercase first: <?php echo ucfirst($third);?>
+        <br />
+        Uppercase words:  <?php echo ucwords($third);?>
+        <br />
     </body>
 </html>
