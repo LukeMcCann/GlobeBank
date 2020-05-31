57d456a8a1ab0d74499963c4d1873c0cd4d8bf49
Luke McCann <developer-lukemccann@outlook.com>
2020-05-31 16:35:27 +0100

    Changes helloworld for multi-line

--- html/helloworld.php
+++ html/helloworld.php
@@ -4,8 +4,6 @@
         <title>Hello World</title>
     </head>
     <body>
-        <?php 
-            echo "Hello World!" . " Where is the bacon?";
-        ?>
+       <?php echo "Hello World!" ?> <br /> <?php echo  " Where is the bacon?" ?>
     </body>
 </html>
