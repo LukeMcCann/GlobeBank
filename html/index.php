3c5577f271eb140e78e6e801c7ce47740400215b
Luke McCann <developer-lukemccann@outlook.com>
2020-05-31 16:54:56 +0100

    Echoing html elements example

--- html/index.php
+++ html/index.php
@@ -1,7 +1,3 @@
 <?php
-  $item = 5;
-  $Item = 3;
 
-  echo $item;
-  echo $Item;
 ?>
---
+++ html/variables.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+    <head>
+        <title>Untitled</title>
+    </head>
+    <body>
+        <?php 
+            $var1 = 10;
+            echo $var1;
+            echo "<br />";
+            $var2 = 20;
+            echo $var2;
+        ?>
+    </body>
+</html>
