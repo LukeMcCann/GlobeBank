da9a01c3f457f8850e0cad33da60dc2a9c7f252d
Luke McCann <developer-lukemccann@outlook.com>
2020-06-01 18:18:35 +0100

    Add basic HTML information to staff pages

--- html/staff/index.php
+++ html/staff/index.php
@@ -2,11 +2,22 @@
 
 <html lang="en">
   <head>
-    <title></title>
+    <title>GBI</title>
     <meta charset="utf-8">
   </head>
 
   <body>
-  
+    <header>
+      <h1>GBI Staff Area</h1>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </nav>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?>Globe Bank
+    </footer>
   </body>
 </html>
