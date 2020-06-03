be37036e1246d99014306e714d0ad54425221c1c
Luke McCann <developer-lukemccann@outlook.com>
2020-06-03 20:03:53 +0100

    Add example of filepath variables to html

--- private/initialize.php
+++ private/initialize.php
@@ -1,5 +1,10 @@
 <?php 
+    # Define constants for paths to this file
+    # Find this files location and get directory
     define("PRIVATE_PATH", dirname(__FILE__));
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
-    require_once('fucntions.php');
+    define("PUBLIC_PATH", PROJECT_PATH . '/public/');
+    define("SHARED_PATH", PRIVATE_PATH . '/shared/');
+
+    require_once('functions.php');
 ?>
