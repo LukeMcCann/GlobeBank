69b5f8539e735045ab472acd763c25d3590e9315
Luke McCann <developer-lukemccann@outlook.com>
2020-06-03 20:08:21 +0100

    Add php href link example

--- html/staff/index.php
+++ html/staff/index.php
@@ -3,6 +3,13 @@
 <?php $page_title = "Staff Menu"; ?>
 <?php include(SHARED_PATH . 'staff_header.php'); ?>
 
-    <div id="content"></div>
+    <div id="content">
+        <div id="main-menu" >
+            <h2>Main Menu</h2>
+            <ul>
+                <li><a href="<?php echo 'subjects/index.php'?>">Subjects</a></li>
+            </ul>
+        </div>
+    </div>
 
 <?php include(SHARED_PATH . 'staff_footer.php'); ?>
