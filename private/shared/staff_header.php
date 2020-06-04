3895c6c0f2a933d38128a28cb10cf5b90686285a
Luke McCann <developer-lukemccann@outlook.com>
2020-06-04 21:46:30 +0100

    Fixes broken stylesheet after url_for implementation

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -8,7 +8,7 @@
   <head>
     <title>GBI - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    <link rel="stylesheet" media="all" href=<?php echo url_for("/stylesheets/staff.css")?> />
   </head>
 
   <body>
@@ -18,6 +18,6 @@
 
     <navigation>
       <ul>
-        <li><a href=<?php echo WWW_ROOT . "index.php"?>>Menu</a></li>
+        <li><a href=<?php echo url_for("/staff/index.php")?>>Menu</a></li>
       </ul>
     </navigation>
