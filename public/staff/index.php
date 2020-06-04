b07e49b03354125006956ef7df2d40562a2b7d10
Luke McCann <developer-lukemccann@outlook.com>
2020-06-04 21:05:52 +0100

    Add php href example

--- public/staff/index.php
+++ public/staff/index.php
@@ -4,6 +4,10 @@
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
+    <h2>Main Menu</h2>
+    <ul>
+        <li><a href=<?php echo "subjects/index.php" ?>>Subjects</a></li>
+    </ul>
 </div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
