8900722af62b632e280b61f38d7a2ff4ffd5d3d1
Luke McCann <developer-lukemccann@outlook.com>
2020-06-05 17:06:39 +0100

    Add htmlspecialchars example

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,12 +1,20 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php 
+
     # ternary conjunction (if isset id = id else id = 1)
     // $id = isset($_GET['id']) ? $_GET['id'] : '1';
     # php7 can do this even simpler using the null coalescing operator
     # check if val there, if it is use it else use second value instead
     $id = $_GET['id'] ?? '1';
-    echo $id;
+
+    # Anytime data is output we need to compensate for HTML special chars.
+    # Cross-site scripting is where JS or other code is injected via the browser
+    # if we don't protect against special chars or reserved characters the browser will read
+    # this as valid code and execute it.
+    echo htmlspecialchars($id);
 ?>
 
-<a href="show.php?name=<?php echo u('John Doe');?>">Link</a><br />
-<a href="show.php?company=<?php echo u('Widgets&More');?>">Link</a><br />
-<a href="show.php?query=<?php echo u('!#*?');?>">Link</a><br />
+<a href="show.php?name=<?php echo urlencode('John Doe');?>">Link</a><br />
+<a href="show.php?company=<?php echo urlencode('Widgets&More');?>">Link</a><br />
+<a href="show.php?query=<?php echo urlencode('!#*?');?>">Link</a><br />
