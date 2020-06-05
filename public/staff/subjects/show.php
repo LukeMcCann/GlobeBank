9002f7076cd122451a83ff7b3a6faf8ad9755822
Luke McCann <developer-lukemccann@outlook.com>
2020-06-05 16:19:23 +0100

    Add null coalescing operator and ternary conjunction examples

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,6 +1,9 @@
 <?php 
-
-    $id = $_GET['id'];
+    # ternary conjunction (if isset id = id else id = 1)
+    // $id = isset($_GET['id']) ? $_GET['id'] : '1';
+    # php7 can do this even simpler using the null coalescing operator
+    # check if val there, if it is use it else use second value instead
+    $page = $_GET['id'] ?? '1';
 
     echo $id;
 ?>
