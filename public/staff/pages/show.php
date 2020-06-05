d6c6c607fad163b1053a9264d6a58632321090a8
Luke McCann <developer-lukemccann@outlook.com>
2020-06-05 18:58:07 +0100

    Adds classes to html to style

--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,15 +1,23 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
-<?php $page_title = 'Pages Menu'; ?>
+<?php
+// $id = isset($_GET['id']) ? $_GET['id'] : '1';
+$id = $_GET['id'] ?? '1'; // PHP > 7.0
+?>
 
-<div class="content">
-    <div id="main-menu">
-        <a href="<? echo url_for('/staff/pages')?>"><< Back to List</a><br />
-        <?php
-            $id = $_GET['id'] ?? '1';
-            echo 'Page ID: ' . htmlspecialchars($id);
-        ?>
-    </div>
+<?php $page_title = 'Show Page'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="page show">
+
+    Page ID: <?php echo htmlspecialchars($id); ?>
+
+  </div>
+
 </div>
+
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
