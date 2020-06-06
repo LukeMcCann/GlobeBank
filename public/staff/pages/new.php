c1fb25ced38f8c0b6b29bfb1f7c85712a4f7b10b
Luke McCann <developer-lukemccann@outlook.com>
2020-06-06 15:17:18 +0100

    Add form to pages

---
+++ public/staff/pages/new.php
@@ -0,0 +1,41 @@
+<?php require_once('../../../private/initialize.php');?>
+
+<?php include(SHARED_PATH . '/staff_header.php');?>
+<?php $page_title = 'Create Subject'; ?>
+<div id="content">
+
+<a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+<div class="subject new">
+  <h1>Create Subject</h1>
+
+  <form action="<?php echo url_for('/staff/subjects/create.php');?>" method="post">
+    <dl>
+      <dt>Menu Name</dt>
+      <dd><input type="text" name="menu_name" value="" /></dd>
+    </dl>
+    <dl>
+      <dt>Position</dt>
+      <dd>
+        <select name="position">
+          <option value="1">1</option>
+        </select>
+      </dd>
+    </dl>
+    <dl>
+      <dt>Visible</dt>
+      <dd>
+      <!-- Once the form is submitted the data sent would be no value whatsoever if visible is
+           not checked. By placing a hidden value it will take presidence if the visible is not
+           checked sending a 0 value, as the checkbox comes later it takes presedence when checked-->
+        <input type="hidden" name="visible" value="0" />
+        <input type="checkbox" name="visible" value="1" />
+      </dd>
+    </dl>
+    <div id="operations">
+      <input type="submit" value="Create Subject" />
+    </div>
+  </form>
+</div>
+</div>
+<?php include(SHARED_PATH . '/staff_footer.php');?>
